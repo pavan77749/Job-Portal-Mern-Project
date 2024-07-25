e1e4c2e13ed14d1b535afe15f6e255dbe3d19b3f
Pavan Gupta <pavangpay05@gmail.com>
2024-07-25 22:53:45 +0530

    created user,job,application,company Model

---
+++ Backend/model/application.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const applicationSchema  = new mongoose.Schema({
+    job:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Job',
+        required:true
+    },
+    applicant:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User',
+        required:true
+    },
+    status:{
+        type:String,
+        enum: ['pending','accepted','rejected'],
+        default:'pending'
+    }
+},{timestamps:true})
+
+export const application = mongoose.model("Application", applicationSchema)
