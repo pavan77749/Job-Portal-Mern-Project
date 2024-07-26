9f17bebcc9f0281ff4d73c00d31acad077fce274
Pavan Gupta <pavangpay05@gmail.com>
2024-07-26 10:58:38 +0530

    Created User Controller (Signup/Login/Update/Logout) functionality, Middleware ,Api routes & Testing

--- Backend/model/company.model.js
+++ Backend/model/company.model.js
@@ -1,27 +1,30 @@
 import mongoose from "mongoose";
 
-const companySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required: true
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    description:{
-        type:String
+    description: {
+      type: String,
     },
-    website:{
-        type:String
+    website: {
+      type: String,
     },
-    location:{
-        type:String
+    location: {
+      type: String,
     },
-    logo:{
-        type:String // URL of company logo
+    logo: {
+      type: String, // URL of company logo
     },
-    userId:{
-        type:mongoose.Schema.ObjectId,
-        ref:'User',
-        required:true
-    }
-},{timestamps:true})
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const company = mongoose.Schema('Company', companySchema)
+export const company = mongoose.Schema("Company", companySchema);
